3714e6beb7e29c0c924c8e848d4528a277999363
Fouad Aoude <57204551+fouadaoude@users.noreply.github.com>
2020-12-17 17:53:49 -0500

    Add files via upload

--- mansfield_program.py
+++ mansfield_program.py
@@ -15,11 +15,18 @@ def connect():
             password="",
             database="mansfield"
         )
-        print("connected successfully")
+        
     except mysql.connector.Error as e:
         print("Something went wrong {}".format(e))
 
     return db
+
+def get_weekday():
+        today = date.today()
+        return calendar.day_name[today.weekday()]
+
+def get_date():
+    return date.today()
 
 
 class Employee:    
@@ -66,8 +73,12 @@ class Employee:
             while True:
                 for employee in employee_info:
                     print('[{id}] {employee}'.format(id=employee[0], employee=employee[1] + ' ' + employee[2]))
+                print('[0] Update checkin for today')
                 option = int(input("Select: "))
                 if isinstance(option, int):
+                    selected_user = employee_info[option-1]
+                    if option == 0:
+                        self.update_check_in(time, selected_user)
                     print("[1] Current time")
                     print("[2] Custom time")
                     time = input("Select: ")
@@ -75,24 +86,62 @@ class Employee:
                         time = self.get_current_datetime()
                     else:
                         time = input("Enter check in time for {employee}: ".format(employee=employee_info[option-1][1]))                                                                                        
-                    print(time)                    
-                    selected_user = employee_info[option-1]
-                    self.insert_check_in(time, selected_user)
-                
+                    print(time)                                        
+                    self.insert_check_in(time, selected_user)                    
+                    break                
         except ValueError:
-            print("Something went wrong")
+            print("Something went wrong")    
+
+    def get_attendance(self, selected_user):
+        if selected_user:
+            
+            try:
+                db = connect()
+                cursor = db.cursor()
+                stmt = ("SELECT * FROM attendance WHERE userID = '{userID}'".format(userID=selected_user))
+                cursor.execute(stmt)
+                attendance = cursor.fetchall()
+
+            except mysql.connector.Error as e:  
+                print("Something went wrong getting attendance from database ERROR: {}".format(e))
+            
+            return attendance
+
+    def check_if_already_inserted(self):
+        
+
+    def update_check_in(self, time, selected_user):
+        if selected_user:
+            print("Would you like to update checkin time for today to {time}?".format(time=time.partition(' ')[2] if len(time.split()) == 3 else time))
+            option = input("Y/N\n")
+            if option.lower() in ['y','yes']: 
+                userID = selected_user[0] if selected_user[0] else 0
+                firstname = selected_user[1] if len(selected_user[1]) > 0 else "Empty"
+                lastname = selected_user[2] if len(selected_user[2]) > 0 else "Empty"
+                checkin = time
+                checkout = "Pending"                   
+                try:
+                    db = connect()
+                    cursor = db.cursor()
+                    stmt = ("UPDATE attendance SET checkin = '{time}' WHERE userID = '{userID}';".format(time=checkin, userID=userID))                                        
+                    cursor.execute(stmt)
+                    db.commit()
+                    clear()
+                    return print("Updated Successfully")
+                except mysql.connector.Error as e:
+                    return print("Error updating checkin field {}".format(e))
 
     def insert_check_in(self, time, selected_user):
         #return all_employees if len(all_employees) > 1 else "Empty"
         if (selected_user):
             #check if there is a userid if there isnt set it to 0 and do not carry on the insertion into db
             userID = selected_user[0] if selected_user[0] else 0
+
             if userID != 0:
                 firstname = selected_user[1] if len(selected_user[1]) > 0 else "Empty"
                 lastname = selected_user[2] if len(selected_user[2]) > 0 else "Empty"
-                checkin = time
+                checkin = get_weekday() + time
                 checkout = "Pending"
-                print(selected_user[1])
                 try:
                     print(checkin)
                     db = connect()
@@ -102,11 +151,13 @@ class Employee:
                     cursor.execute(stmt, val)
                     db.commit()
                     return "Inserted check in time successfully"
-                except ValueError:
-                    print('Something went wrong inserting checkin time into database')
+                except mysql.connector.Error as e:                    
+                    clear()
+                    print("Error {}".format(e))                
             else:
                 print("Missing userID please try again.")
                 return
+            clear()
 
     def get_current_datetime(self):
         return time.strftime('%Y-%m-%d %H:%M:%S')
@@ -125,10 +176,6 @@ class Employee:
 
     def check_in(self, time):
         pass
-
-    def get_today(self):
-        today = date.today()
-        return calendar.day_name[today.weekday()]
 
     def add_employee(self, employee_array):
         firstname = employee_array[0].title() if employee_array else "Empty"
